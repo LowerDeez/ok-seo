0878454d38350de9805dbd6ffd2f827a92bcba64
Oleg <igorkleschunov@gmail.com>
2019-01-21 11:49:30 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='django-ok-seo',
-    version='0.2.1',
+    version='0.2.2',
     description='Django seo app',
     long_description=open('README.md').read(),
     author='Oleg Kleschunov',
