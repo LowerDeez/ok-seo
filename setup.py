a6981d5d3a9d51125fda0b6c4b57d69dc5dfe12a
Oleg <igorkleschunov@gmail.com>
2019-06-24 18:08:47 +0300

    Bump version 0.7.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkj_name = 'seo'
 
 setup(
     name='django-ok-seo',
-    version='0.7.3',
+    version='0.7.4',
     long_description_content_type='text/x-rst',
     packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
     include_package_data=True,
