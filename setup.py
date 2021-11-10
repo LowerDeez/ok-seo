f7af2c1ea82102d35ae5715d7d768cdf68ebf57b
Oleg <igorkleschunov@gmail.com>
2021-11-10 17:06:29 +0200

    Handle cyrillic symbols in path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ pkj_name = 'seo'
 
 setup(
     name='django-ok-seo',
-    version='0.9.2',
+    version='0.9.3',
     long_description_content_type='text/x-rst',
     packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
     include_package_data=True,
