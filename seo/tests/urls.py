780d70cdc701f4d5dce86eadd61bcf8358ce051d
zodman <zodman@gmail.com>
2022-10-07 23:16:48 -0400

    implement modelInstance

--- seo/tests/urls.py
+++ seo/tests/urls.py
@@ -1,6 +1,7 @@
-from django.urls import re_path
+from django.urls import re_path,path,include
 from django.contrib import admin
 
 urlpatterns = [
     re_path(r"^admin/", admin.site.urls),
+     path('pages/', include('django.contrib.flatpages.urls')),
 ]
